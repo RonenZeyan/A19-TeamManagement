2a19fd36b7ab4d89585b0ebca5fc43b6c38a574d
RonenZeyan <101258047+RonenZeyan@users.noreply.github.com>
2024-06-07 10:25:11 +0300

    change case sensitive

--- server/routes/AdminRoute.js
+++ server/routes/AdminRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router()
-const adminController = require('../controllers/adminController.js');
+const adminController = require('../controllers/AdminController.js');
 
 
 /**
